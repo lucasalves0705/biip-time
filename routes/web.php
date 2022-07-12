5619bd3b22772affbfff00388a99ee127e15c860
lucasalves0705 <72676285+lucasalves0705@users.noreply.github.com>
2022-07-12 20:05:23 -0300

    Criação do projeto

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Web\PageController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+
+Route::group(['middleware' => [
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+]], function () {
+    Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+    Route::get('/bater-ponto', [PageController::class, 'toClockIn'])->name('toClockIn');
+});
